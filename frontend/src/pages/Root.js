9d90973d59a74a89d52fac3bee9deee072a73f07
Bahaa <bahaa.bakri1995@gmail.com>
2024-11-28 19:43:11 +0400

    work with auto logout and expied date

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,10 +1,26 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
-
+import { useEffect } from 'react';
+import {getTimeToExpired} from '../util/auth'
 function RootLayout() {
   // const navigation = useNavigation();
-
+  const token = useLoaderData()
+  
+  const timeToExpired = getTimeToExpired()
+  console.log(timeToExpired);
+  
+  const submit = useSubmit()
+  useEffect(() => {
+    if (token && timeToExpired) {
+      setTimeout(() => {
+        submit(null, {
+          action: '/logout',
+          method: 'POST'
+        })
+      },timeToExpired)
+    }
+  }, [token, timeToExpired, submit])
   return (
     <>
       <MainNavigation />
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -3,8 +3,22 @@ import { redirect } from "react-router-dom"
 export function getAuthenticationToken() {
     return localStorage.getItem('token') ?? null
 }
+
+export function getAuthenticationExpiredDate() {
+    return localStorage.getItem('expireDateInTS') ?? null
+}
+
+export function getTimeToExpired() {
+    if (!getAuthenticationExpiredDate()) {
+        return null
+    }
+    return new Date().getTime() - getAuthenticationExpiredDate().getTime()
+}
 export function setAuthenticationToken(token) {
     localStorage.setItem('token', token)
+    const expireDateInDate = new Date()
+    expireDateInDate.setHours(expireDateInDate.getHours() + 1)
+    localStorage.setItem('expireDateInTS', expireDateInDate.toISOString())
 }
 export function deleteAuthenticationToken() {
     localStorage.removeItem('token')
